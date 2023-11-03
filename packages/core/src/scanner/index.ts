8b7d148620252c3b13d7c46d70a1f322866c8b19
Ayoub Adib <adbayb@gmail.com>
2023-11-04 00:33:01 +0100

    feat: ignore test and story-like files with the ability to customize it

--- packages/core/src/scanner/index.ts
+++ packages/core/src/scanner/index.ts
@@ -3,39 +3,51 @@ import { dirname } from "node:path";
 
 import { CWD } from "../constants";
 
-type ProjectFileStructure = {
-	root: string;
-	metadata: string;
-	files: string[];
+export type ScannerOptions = {
+	/**
+	 * A list of folders to ignore
+	 *
+	 * @default [".git", "node_modules", "dist", "out"]
+	 */
+	excludeFolders?: string[];
+	/**
+	 * A list of files to include (following glob matcher)
+	 *
+	 * @default **\/*\/!(test|stories|*.test|*.stories).?(m){j,t}s?(x)
+	 */
+	includeFiles?: string[];
 };
 
-export const scan = () => {
-	const projects = getProjects();
+export const scan = (options: ScannerOptions = {}) => {
+	const excludedFolders = options.excludeFolders ?? DEFAULT_EXCLUDED_FOLDERS;
+	const includedFiles = options.includeFiles ?? DEFAULT_INCLUDED_FILES;
 
-	const output: ProjectFileStructure[] = projects.map((project) => {
-		return { ...project, files: getFiles(project.root) };
-	});
-
-	return output;
-};
-
-const getProjects = (): Pick<ProjectFileStructure, "metadata" | "root">[] => {
-	return new fdir()
+	const projects = new fdir()
 		.withBasePath()
 		.glob("**/package.json")
+		.exclude((dirName) => excludedFolders.includes(dirName))
 		.crawl(CWD)
 		.sync()
 		.map((path) => {
-			return { root: dirname(path), metadata: path };
+			return { metadata: path, folder: dirname(path) };
 		});
+
+	return projects.map((project) => {
+		const files: string[] = new fdir()
+			.withBasePath()
+			.glob(...includedFiles)
+			.exclude((dirName) => excludedFolders.includes(dirName))
+			.crawl(project.folder)
+			.sync();
+
+		return { ...project, files };
+	});
 };
 
-const getFiles = (projectPath: string) => {
-	return new fdir()
-		.withBasePath()
-		.glob("**/*.?(m){j,t}s?(x)") // js|jsx|ts|tsx|cjs|mjs|cjsx|mjsx
-		.crawl(projectPath)
-		.sync();
-};
+const DEFAULT_EXCLUDED_FOLDERS = [".git", "node_modules", "dist", "out"];
+
+const DEFAULT_INCLUDED_FILES = [
+	"**/*/!(test|*.test|stories|*.stories).?(m){j,t}s?(x)", // js|jsx|ts|tsx|cjs|mjs|cjsx|mjsx excluding test-like and story-like files
+];
 
 console.log(scan());
