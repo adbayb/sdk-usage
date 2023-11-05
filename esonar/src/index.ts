a37f9d6d4d18ec007a90a37a32cca92d88511573
Ayoub Adib <adbayb@gmail.com>
2023-11-05 22:52:24 +0100

    refactor: move cli logic into core

--- esonar/src/index.ts
+++ esonar/src/index.ts
@@ -1,34 +1,7 @@
-import type { Item } from "@esonar/core";
-import { parse, scan } from "@esonar/core";
-import { readFileSync } from "fs";
+import { esonar } from "@esonar/core";
 
 const main = async () => {
-	const projects = scan();
-	const items: Item[] = [];
-
-	for (const project of projects) {
-		for (const file of project.files) {
-			const content = readFileSync(file, "utf-8");
-			const module = project.metadata.name;
-
-			// console.log(
-			// 	file,
-			// 	await parse(content, {
-			// 		file,
-			// 		module,
-			// 	}),
-			// );
-
-			items.push(
-				...(await parse(content, {
-					file,
-					module,
-				})),
-			);
-		}
-	}
-
-	console.log(items);
+	await esonar();
 };
 
 main().catch((error) => {
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,26 @@
-export * from "./parser";
-export * from "./scanner";
+import { readFileSync } from "fs";
+
+import { scan } from "./scanner";
+import type { Item } from "./entities/item";
+import { parse } from "./parser";
+
+export const esonar = async () => {
+	const projects = scan();
+	const items: Item[] = [];
+
+	for (const project of projects) {
+		for (const file of project.files) {
+			const content = readFileSync(file, "utf-8");
+			const module = project.metadata.name;
+
+			const projectItems = await parse(content, {
+				file,
+				module,
+			});
+
+			items.push(...projectItems);
+		}
+	}
+
+	console.log(items);
+};
